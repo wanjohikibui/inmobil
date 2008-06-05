7f868ec3fb223463497a1a1a5600fc1bd1e1509b
gaitan <gaitan@4c00e181-e34e-0410-8fc5-c3925a952a90>
2008-06-05 05:57:37 +0000

    editado url para mapear el directorio de media

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ urlpatterns = patterns('',
     # Example:
     # (r'^inmobil/', include('inmobil.foo.urls')),
 
+ 	(r'^media/(.*)$','django.views.static.serve',{'document_root': 'upload/', 'show_indexes': True})
+
     # Uncomment this for admin:
      (r'^admin/', include('django.contrib.admin.urls')),
 )
