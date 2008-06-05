25750ddd5e67cffb1b7f82a89d951001f9dde028
gaitan <gaitan@4c00e181-e34e-0410-8fc5-c3925a952a90>
2008-06-05 08:53:32 +0000

    cambiando vista de lugar

--- views.py
+++ views.py
@@ -3,13 +3,19 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django import newforms as forms
-from inmobil.balance.models import Administradora
+from inmobil.balance.models import *
+
 
 def index(request):
 	"""vista de la pagina inicial del sistema"""
 	administradora = Administradora.objects.get(id=1) #selecciono la 1º administrad **solo para probar
 	return render_to_response('index.html', {'admin': administradora})
 
+
+def listarConsorcios(request):
+    m_list = Consorcio.objects.all()
+    return render_to_response('consorcios.html',{'m_list': m_list})
+	
 
 
 
