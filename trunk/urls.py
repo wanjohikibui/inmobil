51f98e1850d169a066079e1af4b5e28f40d377e4
gaitan <gaitan@4c00e181-e34e-0410-8fc5-c3925a952a90>
2008-06-05 06:20:50 +0000

    typo en urls.py

--- trunk/urls.py
+++ trunk/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
     # Example:
     # (r'^inmobil/', include('inmobil.foo.urls')),
 
- 	(r'^media/(.*)$','django.views.static.serve',{'document_root': 'upload/', 'show_indexes': True})
+ 	(r'^media/(.*)$','django.views.static.serve',{'document_root': 'upload/', 'show_indexes': True}),
 
     # Uncomment this for admin:
      (r'^admin/', include('django.contrib.admin.urls')),
