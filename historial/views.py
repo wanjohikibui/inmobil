07233d72f22a7d24798b8d144cdf62d0db8b3306
federicoperetti <federicoperetti@4c00e181-e34e-0410-8fc5-c3925a952a90>
2008-06-06 22:17:15 +0000

    Historial de pagos

--- historial/views.py
+++ historial/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
 from inmobil.historial.models import Pago
-from inmobil.balance.models import *
+from django.http import Http404
+from django.template import Template
+from django.template import TemplateDoesNotExist
+from inmobil.balance.models import * 
+from django.shortcuts import render_to_response
 
-def pago_detail(request, depto_id, consorcio_id):
+def pago_detail(request, year, day,month, consorcio_id):
     """muestra el detalle del pago por departamento"""
     consorcio = Consorcio.objects.get(id=consorcio_id)
     deptos = Depto.objects.filter(consorcio=consorcio_id)
-    monto = Pago.objects.filter(depto=depto_id)
-    fecha_de_pago = Pago.objects.get(
-    
-    
-    return render_to_response('historial/pago_detail.html', {'consorcio': consorcio, 
-                            'deptos':deptos, 'monto':monto, 'fecha_de_pago' : fecha})
+    monto = Pago.objects.filter(balance=consorcio_id)
+    return render_to_response('balance/pago_detail.html', {'consorcio': consorcio,                             'deptos':deptos, 'monto':monto})
