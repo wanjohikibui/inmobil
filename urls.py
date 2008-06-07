48f04861ad7f291f290d9768ef5c80e5b18a210f
federicoperetti <federicoperetti@4c00e181-e34e-0410-8fc5-c3925a952a90>
2008-06-07 06:09:56 +0000

    Añadido listar balance por departamentos, ej: http://127.0.0.1:8000/consorcio/1/depto3-D Deberia listar el balance del departamento 3-D del consorcio 1. Todavia faltan implementar algunas cosas a mostrar. Modificado urls.py para que depto_balance_detail funcione correctamente.

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@
 #coding=utf-8
 from django.conf.urls.defaults import *
 from django.views.generic import list_detail 
-from inmobil.balance.models import Consorcio, Administradora
+from inmobil.balance.models import Consorcio, Administradora, Depto
 from inmobil.views import *
 
 
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^consorcio/(?P<consorcio_id>\d+)/(?P<year>\d{4})-(?P<month>\d{2})/$', 'inmobil.balance.views.balance_detail'),    
     (r'^consorcio/(?P<consorcio_id>\d+)/balance/new', 'inmobil.balance.views.balance_new'),
     (r'^consorcio/(?P<consorcio_id>\d+)/balance/(?P<balance_id>\d+)', 'inmobil.balance.views.balance_add_modify'),
-
+    (r'^consorcio/(?P<consorcio_id>\d+)/depto(?P<piso_id>)', 'inmobil.balance.views.depto_balance_detail'),
     
 
     #(r'^consorcios/$', 'inmobil.views.listarConsorcios'),     
